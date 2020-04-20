e0dbfcac82b8500d4b6e937afb480088ccb3fdaa
Urielfernandez <Urielfernandezgranha@gmail.com>
2020-04-20 21:38:07 +0200

    Generación de scripts para la db

--- SQL/scriptBD.sql
+++ SQL/scriptBD.sql
@@ -6,14 +6,70 @@ CREATE DATABASE tiendacds;
 
 USE tiendacds;
 
-CREATE TABLE usuarios
-(
-	id integer(11) auto_increment primary key,
-	nombre varchar(45) not null,
-	apellidos varchar(45) not null,
-	email varchar(45) not null
+-- Tabla cds
+
+CREATE TABLE cds (
+  titulo varchar(100) NOT NULL,
+  artista varchar(100) NOT NULL,
+  pais varchar(100) NOT NULL,
+  precio varchar(100) NOT NULL,
+  anho varchar(4) NOT NULL,
+  PRIMARY KEY (titulo)
 );
 
+
+-- Tabla inventario
+
+CREATE TABLE inventario (
+  cd varchar(100) NOT NULL,
+  cantidad_stock int NOT NULL,
+  PRIMARY KEY (cd),
+  FOREIGN KEY (cd) REFERENCES cds(titulo)
+);
+
+-- Tabla usuarios
+
+CREATE TABLE usuarios (
+  email varchar(100) NOT NULL,
+  nombre varchar(100) NOT NULL,
+  PRIMARY KEY (email)
+);
+
+
+-- Tabla pedidos
+
+CREATE TABLE pedidos (
+  id int NOT NULL AUTO_INCREMENT,
+  usuario varchar(100) NOT NULL,
+  total_compra float NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (usuario) REFERENCES usuarios(email)
+);
+
+-- Tabla opiniones
+
+CREATE TABLE opiniones (
+    nota float NOT NULL,
+    opinion varchar(300) NOT NULL,
+    cd varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    PRIMARY KEY(cd,email),
+    FOREIGN KEY (cd) REFERENCES cds(titulo),
+    FOREIGN KEY (email) REFERENCES usuarios(email)
+);
+
+
+-- Tabla items_pedido
+
+CREATE TABLE items_pedido (
+  pedido int NOT NULL,
+  cd varchar(100) NOT NULL,
+  cantidad int NOT NULL,
+  fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (pedido,cd),
+  FOREIGN KEY (cd) REFERENCES cds(titulo),
+  FOREIGN KEY (pedido) REFERENCES pedidos(id)
+);
 
 CREATE USER 'dawa'@'localhost' IDENTIFIED BY 'Dawaproyecto1';
 
@@ -21,5 +77,3 @@ GRANT ALL PRIVILEGES ON tiendacds . * TO 'dawa'@'localhost';
 
 FLUSH PRIVILEGES;
 
-INSERT INTO altadaw (nombre, apellidos, email)
-VALUES ("Manuel", "Gonzalez Costa", "email1@email.com");
