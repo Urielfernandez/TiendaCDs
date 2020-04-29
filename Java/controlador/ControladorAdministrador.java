541ce0dcb155bc1b433e51c0bc9d1e8114a3d926
Damian <damian.cruz@rai.usc.es>
2020-04-29 21:05:15 +0200

    Empiezo doget controladorAdministrador

--- Java/controlador/ControladorAdministrador.java
+++ Java/controlador/ControladorAdministrador.java
@@ -1,8 +1,13 @@
 package controlador;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import modelo.tienda.Carrito;
+import modelo.vo.UsuarioVO;
 
 public class ControladorAdministrador extends HttpServlet {
 
@@ -11,6 +16,45 @@ public class ControladorAdministrador extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        HelperCD gestionCDS;
+        Carrito carrito;
+        UsuarioVO usuario;
+        Connection conexion;
+        /*
+         * Cuando el usuario inicio se debería: Generar la sesion del usuario Generar
+         * una conexion a la BD y almacenarla en dicha sesion
+         */
+
+        // Obtenemos la sesion y la creamos si no la hay
+		HttpSession sesion = request.getSession();
+
+        if (sesion.getAttribute("usuario") == null && sesion.getAttribute("carrito") == null
+                && sesion.getAttribute("conexion") == null) {
+			// Creamos el usuario y el carrito para una sesion
+			sesion.setAttribute("usuario", new UsuarioVO());
+            sesion.setAttribute("carrito", new Carrito());
+            
+            Connection aux = crearConexionBBDD();
+
+            // Comprobamos si se conecta, si no mostramos un error
+            if (aux != null) {
+                sesion.setAttribute("conexion", aux);
+            }
+            else {
+                mostrarPagina("jsp/error.jsp", request, response);
+            }
+		}
+
+		// Obtenemos el usuario y el carrito de la sesion
+		carrito = (Carrito) sesion.getAttribute("carrito");
+        usuario = (UsuarioVO) sesion.getAttribute("usuario");
+        conexion = (Connection) sesion.getAttribute("conexion");
+
+        gestionCDS = new HelperCD();  
+
+        request.setAttribute("listaArticulos", gestionCDS.cargarCDs(conexion));
+
         mostrarPagina("index.html", request, response);
     }
 
@@ -22,5 +66,22 @@ public class ControladorAdministrador extends HttpServlet {
             throws ServletException, IOException {
         request.getRequestDispatcher(pagina).forward(request, response);
     }
+    private Connection crearConexionBBDD() {
+        Connection conexion = null;
 
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/tiendacds?serverTimezone=UTC", "dawa", "Dawaproyecto1");
+        } 
+        catch (SQLException e) {
+            System.out.println("Controlador Tienda: no se ha podido generar una conexión para el usuario.");
+            System.out.println(e.getMessage());
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Controlador Tienda: no se ha podido cargar el driver.");
+            System.out.println(e.getMessage());
+        }
+
+        return conexion;
+    }
 }
