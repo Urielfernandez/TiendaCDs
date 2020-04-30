264d0e5225256a70e5cb2990e10a33551f1fe07a
gonzalezcostamanuel <gonzalezcostamanuel@gmail.com>
2020-04-30 17:12:25 +0200

    Añadidos usuariosVip y Basicos

---
+++ Java/modelo/vo/UsuarioVIP.java
@@ -0,0 +1,13 @@
+package modelo.vo;
+
+public class UsuarioVIP extends UsuarioVO{
+
+    public UsuarioVIP() {
+    }
+
+    public UsuarioVIP(String nombre, String email) {
+        super(nombre, email);
+    }
+
+    
+}
