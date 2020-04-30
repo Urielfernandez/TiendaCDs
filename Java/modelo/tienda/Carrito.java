b72750d931cf117d02a705c8c5bf76eb93dbf5b2
Urielfernandez <urielfernandezgranha@gmail.com>
2020-04-30 18:43:03 +0200

    Funcionalidad de eliminar del carrito terminada

--- Java/modelo/tienda/Carrito.java
+++ Java/modelo/tienda/Carrito.java
@@ -24,6 +24,12 @@ public class Carrito {
         }
     }
 
+    public void eliminarDelCarrito(String titulo){
+        if(this.productos.containsKey(titulo)){
+            this.productos.remove(titulo);
+        }
+    }
+
 
     public HashMap<String,Seleccion> getProductos() {
         return this.productos;
